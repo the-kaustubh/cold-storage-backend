aa648301ab0b619915845a1bf85598d58defe4a5
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2020-11-27 22:51:46 +0530

    lint & remove lints from ci

--- test/index.js
+++ test/index.js
@@ -1,20 +1,20 @@
-const chai = require("chai")
-const chaiHttp = require("chai-http")
-const app = require("../index")
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../index");
 
-const { expect } = chai
+const { expect } = chai;
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
 const mockUser = {
-	username: 'kaustubh',
-	email: 'kaustubh.murumkar@gmail.com',
-	password: 'blahblah',
-	institute: 'jsom',
-	designation: 'user'
-}
+	username: "kaustubh",
+	email: "kaustubh.murumkar@gmail.com",
+	password: "blahblah",
+	institute: "jsom",
+	designation: "user"
+};
 
-let accTok = ''
+let accTok = "";
 
 const mockNode = {
 	"uid": "100000",
@@ -37,7 +37,7 @@ const mockNode = {
 			"b": "0"
 		}
 	}
-}
+};
 
 describe("construct users", () => {
 	// User should not exist
@@ -45,20 +45,20 @@ describe("construct users", () => {
 		it("should return a error message", done => {
 			chai
 				.request(app)
-				.post('/user/login')
+				.post("/user/login")
 				.send({
 					username: mockUser.username,
 					password: mockUser.password
 				})
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(400)
-					expect(res.body).to.be.an('object')
-					expect(res.body).to.have.property('message')
-					done()
-				})
-		})
-	})
+					if (err) done(err);
+					expect(res).to.have.status(400);
+					expect(res.body).to.be.an("object");
+					expect(res.body).to.have.property("message");
+					done();
+				});
+		});
+	});
 
 	// Create user
 	describe("POST /user/register", () => {
@@ -68,18 +68,18 @@ describe("construct users", () => {
 				.post("/user/register")
 				.send(mockUser)
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(200)
-					expect(res).to.be.a('object')
-					expect(res.body).to.have.property('username')
-					expect(res.body).to.have.property('email')
-					expect(res.body).to.have.property('password')
-					expect(res.body).to.have.property('institute')
-					expect(res.body).to.have.property('designation')
-					done()
-				})
-		})
-	})
+					if (err) done(err);
+					expect(res).to.have.status(200);
+					expect(res).to.be.a("object");
+					expect(res.body).to.have.property("username");
+					expect(res.body).to.have.property("email");
+					expect(res.body).to.have.property("password");
+					expect(res.body).to.have.property("institute");
+					expect(res.body).to.have.property("designation");
+					done();
+				});
+		});
+	});
 
 	// Login User
 	describe("POST /user/login", () => {
@@ -92,57 +92,57 @@ describe("construct users", () => {
 					password: mockUser.password
 				})
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(200)
-					expect(res).to.be.an('object')
-					expect(res.body).to.have.property('accessToken')
-					accTok = res.body.accessToken
-					expect(res.body).to.have.property('designation')
-					done()
-				})
-		})
-	})
+					if (err) done(err);
+					expect(res).to.have.status(200);
+					expect(res).to.be.an("object");
+					expect(res.body).to.have.property("accessToken");
+					accTok = res.body.accessToken;
+					expect(res.body).to.have.property("designation");
+					done();
+				});
+		});
+	});
 
 	// Login with wrong password
 	describe("POST /user/login", () => {
 		it("should return a error message", done => {
 			chai
 				.request(app)
-				.post('/user/login')
+				.post("/user/login")
 				.send({
 					username: mockUser.username,
-					password: 'xyz'
+					password: "xyz"
 				})
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(200)
-					expect(res.body).to.be.an('object')
-					expect(res.body).to.have.property('message')
-					done()
-				})
-		})
-	})
+					if (err) done(err);
+					expect(res).to.have.status(200);
+					expect(res.body).to.be.an("object");
+					expect(res.body).to.have.property("message");
+					done();
+				});
+		});
+	});
 
 	// Login with wrong username and password
 	describe("POST /user/login", () => {
 		it("should return a error message", done => {
 			chai
 				.request(app)
-				.post('/user/login')
+				.post("/user/login")
 				.send({
-					username: 'abcd',
-					password: 'xyz'
+					username: "abcd",
+					password: "xyz"
 				})
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(400)
-					expect(res.body).to.be.an('object')
-					expect(res.body).to.have.property('message')
-					done()
-				})
-		})
-	})
-})
+					if (err) done(err);
+					expect(res).to.have.status(400);
+					expect(res.body).to.be.an("object");
+					expect(res.body).to.have.property("message");
+					done();
+				});
+		});
+	});
+});
 
 describe("nodes", () => {
 	describe("GET /node", () => {
@@ -151,12 +151,12 @@ describe("nodes", () => {
 				.request(app)
 				.get("/node")
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(401)
-					done()
-				})
-		})
-	})
+					if (err) done(err);
+					expect(res).to.have.status(401);
+					done();
+				});
+		});
+	});
 
 	describe("GET /node", () => {
 		it("should return empty array", (done) => {
@@ -164,16 +164,16 @@ describe("nodes", () => {
 				.request(app)
 				.get("/node")
 				.set({
-					'Authorization': 'Bearer ' + accTok
+					"Authorization": "Bearer " + accTok
 				})
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(200)
-					expect(res.body).to.be.an('array').that.is.empty
-					done()
-				})
-		})
-	})
+					if (err) done(err);
+					expect(res).to.have.status(200);
+					expect(res.body).to.be.an("array").that.is.empty;
+					done();
+				});
+		});
+	});
 
 	describe("POST /node/add", () => {
 		it("should add a new node", done => {
@@ -182,20 +182,20 @@ describe("nodes", () => {
 				.post("/node/add")
 				.send(mockNode)
 				.set({
-					'Authorization': 'Bearer ' + accTok
+					"Authorization": "Bearer " + accTok
 				})
 				.end((err, res) => {
-					if(err) done(err)
-					expect(res).to.have.status(201)
-					expect(res.body).to.be.an('object')
-					expect(res.body).to.have.property('node')
-					expect(res.body).to.have.property('reading')
-					expect(res.body.node).to.have.property('user')
-					expect(res.body.node.user).to.equal(mockUser.username)
-					done()
-				})
-		})
-	})
+					if(err) done(err);
+					expect(res).to.have.status(201);
+					expect(res.body).to.be.an("object");
+					expect(res.body).to.have.property("node");
+					expect(res.body).to.have.property("reading");
+					expect(res.body.node).to.have.property("user");
+					expect(res.body.node.user).to.equal(mockUser.username);
+					done();
+				});
+		});
+	});
 
 	describe("GET /node", () => {
 		it("should return previously added node", (done) => {
@@ -203,17 +203,17 @@ describe("nodes", () => {
 				.request(app)
 				.get("/node")
 				.set({
-					'Authorization': 'Bearer ' + accTok
+					"Authorization": "Bearer " + accTok
 				})
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(200)
-					expect(res.body).to.be.an('array')
-					expect(res.body.length).to.equal(1)
-					done()
-				})
-		})
-	})
+					if (err) done(err);
+					expect(res).to.have.status(200);
+					expect(res.body).to.be.an("array");
+					expect(res.body.length).to.equal(1);
+					done();
+				});
+		});
+	});
 
 	describe("DELETE /node/uid", () => {
 		it("should delete the created node", done => {
@@ -221,17 +221,17 @@ describe("nodes", () => {
 				.request(app)
 				.delete("/node/"+mockNode.uid)
 				.set({
-					'Authorization': 'Bearer ' + accTok
+					"Authorization": "Bearer " + accTok
 				})
 				.end((err, res) => {
-					if(err) done(err)
-					expect(res).to.have.status(200)
-					expect(res.body).to.have.property("message")
-					expect(res.body.message).to.equal("Deleted Successfully")
-					done()
-				})
-		})
-	})
+					if(err) done(err);
+					expect(res).to.have.status(200);
+					expect(res.body).to.have.property("message");
+					expect(res.body.message).to.equal("Deleted Successfully");
+					done();
+				});
+		});
+	});
 
 	describe("GET /node", () => {
 		it("should again return empty array", (done) => {
@@ -239,15 +239,15 @@ describe("nodes", () => {
 				.request(app)
 				.get("/node")
 				.set({
-					'Authorization': 'Bearer ' + accTok
+					"Authorization": "Bearer " + accTok
 				})
 				.end((err, res) => {
-					if (err) done(err)
-					expect(res).to.have.status(200)
-					expect(res.body).to.be.an('array').that.is.empty
-					done()
-				})
-		})
-	})
-})
+					if (err) done(err);
+					expect(res).to.have.status(200);
+					expect(res.body).to.be.an("array").that.is.empty;
+					done();
+				});
+		});
+	});
+});
 
