00f194cadc3e7a977cff7d7821ff16448c61323f
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2020-12-06 01:13:50 +0530

    Add write readings endpoint

--- routes/readings.js
+++ routes/readings.js
@@ -1,27 +1,34 @@
-const express = require("express");
-const router  = express.Router();
-// const Node    = require("../models/readings");
+const express    = require("express");
+const router     = express.Router();
+const Reading    = require("../models/readings");
+const authToken  = require("../authToken.js");
 
-router.get("/:uid", async (req, res) => {
-	console.log(req.params.uid);
-	res.send("alsdfj");
+router.get("/:uid", authToken, async (req, res) => {
+	// TODO: implement and return the latest readings
+	let reading;
+	try {
+		reading = await Reading.find({
+			uid: req.params.uid
+		}).sort({
+			datetime:-1
+		});
+		res.json(reading[0]);
+	} catch(err) {
+		res.status(500).json({message: err.message});
+	}
 });
 
-// router.post("/write", async (req, res) => {
-// });
-
-// async function getNode(req, res, next) {
-// 	let node;
-// 	try {
-// 		node = await Node.find({"uid": req.body.uid});
-// 		if(node == null) {
-// 			return res.status(404).json({ message: "Cannot Find Node"});
-// 		}
-// 	} catch(err) {
-// 		return res.status(404).json({ message: err.message});
-// 	}
-// 	res.node = node[0];
-// 	next();
-// }
+router.post("/write", async (req, res) => {
+	try {
+		let reading = new Reading(req.body);
+		let newReading = reading.save();
+		if(newReading == null) {
+			throw new Error("Couldn't Save.");
+		}
+		res.status(200).json({msg: "OK"});
+	} catch(err) {
+		res.status(500).json({message: err.message});
+	}
+});
 
 module.exports = router;
